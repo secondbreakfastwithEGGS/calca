783eac27d2745d6cae3283cb6204734187613876
secondbreakfastwithEGGS <slanzar2031@wvc.pshs.edu.ph>
2026-09-02 16:41:52 +0800

    Update LANZAR_SEYRAZ_.py
    Added a comment to clarify the output of the distance calculation.

--- LANZAR_SEYRAZ_.py
+++ LANZAR_SEYRAZ_.py
@@ -4,11 +4,15 @@ point_x1 = float(input("enter x1: "))
 point_x2 = float(input("enter x2: "))
 point_y1 = float(input("enter y1: "))
 point_y2 = float(input("enter y2: "))
+#asks to input values for calculation
 
 point_x = point_x2 - point_x1
 point_y = point_y2 - point_y1
+#calculates values
 
 point_xy = pow(point_x, 2) + pow(point_y,2)
+#using calculated values to calculate for the distance
 
 distance = math.sqrt(point_xy)
 print("the distance is ", distance)
+#tells you the final distance value
